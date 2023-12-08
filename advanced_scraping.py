a1aa6f4559fc9d7130fc42a813808faf25978f1e
Varelion <business.8508@gmail.com>
2023-12-08 08:36:38 -0500

    Implemented data-saving measures to scraping code

---
+++ advanced_scraping.py
@@ -0,0 +1,39 @@
+# Regular Expressions
+import re
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://en.wikipedia.org/wiki/Bristlecone_pine"
+path = "/home/z/python_webscraping_class/saved_page1.html"
+saved_html = False
+
+def get_html(url, path):
+    if (saved_html == True):
+        print("Already have something in there")
+    else:
+        response = requests.get(url)
+        with open(path, "w", encoding="utf-8") as f:
+            f.write(response.text)
+
+try:
+    with open(path, "r", encoding='utf-8') as f:
+        saved_html = f.read()
+except FileNotFoundError:
+    print("File not found!!!")
+    get_html(url, path)
+    print("File now has been saved, program will not shut down.")
+    # Next line stops the program from executing.
+    sys.exit()
+except Exception as e:
+    print("Error!:", e)
+
+
+print("End")
+
+# this saves the file on the path read as utf-8 as f
+# then it saves html variable as the contents being read from object f
+
+
+
+###
